6a1bb32964652c7cdba1a1e86876af8c9cfba166
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-07 16:03:21 -0700

    added forEach loop to pull in titles, timestamp, and coordinates for earthquakes

--- scripts/app.js
+++ scripts/app.js
@@ -14,15 +14,22 @@ $(document).ready(function() {
 
   function onSuccess(responseData){
     //title
-    $('#info').append($(`<h2> ${responseData.features[0].properties.title} </h2>`));
-    responseData.features[0].properties.title;
-    console.log(responseData.features[0].properties.title);
-    //coordinates
-    responseData.features[0].geometry.coordinates;
-    console.log(responseData.features[0].geometry.coordinates);
-    //time
-    responseData.features[0].properties.time;
-    console.log(responseData.features[0].properties.time);
+
+    // $('#info').append($(`<h2> ${responseData.features[0].properties.title} </h2>`));
+    // responseData.features[0].properties.title;
+    // console.log(responseData.features[0].properties.title);
+    // //coordinates
+    // responseData.features[0].geometry.coordinates;
+    // console.log(responseData.features[0].geometry.coordinates);
+    // //time
+    // responseData.features[0].properties.time;
+    // console.log(responseData.features[0].properties.time);
+
+    responseData.features.forEach(function (event, i){
+      $('#info').append($(`<h4> ${responseData.features[i].properties.title} </h4>`));
+      $('#info').append($(`<h4> ${responseData.features[i].properties.time} </h4>`));
+      $('#info').append($(`<h4> ${responseData.features[i].geometry.coordinates} </h4>`));
+    })
   }
 
   earthquakeList();
