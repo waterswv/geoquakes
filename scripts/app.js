296f726a6b9472df3fcd8ab9cbe12a8136fd0d1c
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-07 16:59:56 -0700

    added lat long data from first 10 equakes to js file

--- scripts/app.js
+++ scripts/app.js
@@ -24,34 +24,46 @@ $(document).ready(function() {
     // //time
     // responseData.features[0].properties.time;
     // console.log(responseData.features[0].properties.time);
+    let theLocation = {
+      lat: 37.78,
+      lng: -122.44
+    };
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 3,
+      center: theLocation
+    });
 
     responseData.features.forEach(function(event, i) {
       $('#info').append($(`<p> ${responseData.features[i].properties.title} </p>`));
       //$('#info').append($(`<p> ${responseData.features[i].properties.time} </p>`));
       //$('#info').append($(`<p> ${responseData.features[i].geometry.coordinates} </p>`));
+      let theLocation = $(responseData.features[i].geometry.coordinates);
+      let latLng = new google.maps.LatLng(theLocation[1], theLocation[0]);
+      console.log(theLocation);
+      let marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
     })
 
     // Added a map via the Google Maps API ... there is a map class div in our index.html &
     //  a javascript import tag in the head with our google maps api key
 
-    var theLocation = {lat: 37.78, lng: -122.44};
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 8,
-              center: theLocation
-            });
-            var marker = new google.maps.Marker({
-              position: theLocation,
-              map: map
-            });
+    // var theLocation = {lat: 37.78, lng: -122.44};
+    //
+    // var marker = new google.maps.Marker({
+    //   position: theLocation,
+    //   map: map
+    // });
 
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: {
-  //       lat: 37.78,
-  //       lng: -122.44
-  //     },
-  //     zoom: 8
-  //   });
-  //
+    //   map = new google.maps.Map(document.getElementById('map'), {
+    //     center: {
+    //       lat: 37.78,
+    //       lng: -122.44
+    //     },
+    //     zoom: 8
+    //   });
+    //
   }
 
   earthquakeList();
