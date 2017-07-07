da7b0bc7d265d13867539936fe9516bc19599c64
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-07 16:29:28 -0700

    added a map from he google maps api with a pin in GA SF location coordinates

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,15 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
-  function earthquakeList(){
+  function earthquakeList() {
     $.ajax({
       method: "GET",
-      url:"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-06-30&endtime=2017-07-06&limit=10",
+      url: "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-06-30&endtime=2017-07-06&limit=10",
       success: onSuccess
     });
-    }
+  }
 
-  function onSuccess(responseData){
+  function onSuccess(responseData) {
     //title
 
     // $('#info').append($(`<h2> ${responseData.features[0].properties.title} </h2>`));
@@ -25,11 +25,33 @@ $(document).ready(function() {
     // responseData.features[0].properties.time;
     // console.log(responseData.features[0].properties.time);
 
-    responseData.features.forEach(function (event, i){
-      $('#info').append($(`<h4> ${responseData.features[i].properties.title} </h4>`));
-      $('#info').append($(`<h4> ${responseData.features[i].properties.time} </h4>`));
-      $('#info').append($(`<h4> ${responseData.features[i].geometry.coordinates} </h4>`));
+    responseData.features.forEach(function(event, i) {
+      $('#info').append($(`<p> ${responseData.features[i].properties.title} </p>`));
+      //$('#info').append($(`<p> ${responseData.features[i].properties.time} </p>`));
+      //$('#info').append($(`<p> ${responseData.features[i].geometry.coordinates} </p>`));
     })
+
+    // Added a map via the Google Maps API ... there is a map class div in our index.html &
+    //  a javascript import tag in the head with our google maps api key
+
+    var theLocation = {lat: 37.78, lng: -122.44};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 8,
+              center: theLocation
+            });
+            var marker = new google.maps.Marker({
+              position: theLocation,
+              map: map
+            });
+
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {
+  //       lat: 37.78,
+  //       lng: -122.44
+  //     },
+  //     zoom: 8
+  //   });
+  //
   }
 
   earthquakeList();
