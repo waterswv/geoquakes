8ce47dc6d6a0314d4e9aeefc2753e197eab75efe
NrupM <maryenorthrup@gmail.com>
2017-07-07 15:49:08 -0700

    generated javascript to pull API object and added title to page

--- scripts/app.js
+++ scripts/app.js
@@ -4,5 +4,26 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+  function earthquakeList(){
+    $.ajax({
+      method: "GET",
+      url:"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2017-06-30&endtime=2017-07-06&limit=10",
+      success: onSuccess
+    });
+    }
 
+  function onSuccess(responseData){
+    //title
+    $('#info').append($(`<h2> ${responseData.features[0].properties.title} </h2>`));
+    responseData.features[0].properties.title;
+    console.log(responseData.features[0].properties.title);
+    //coordinates
+    responseData.features[0].geometry.coordinates;
+    console.log(responseData.features[0].geometry.coordinates);
+    //time
+    responseData.features[0].properties.time;
+    console.log(responseData.features[0].properties.time);
+  }
+
+  earthquakeList();
 });
